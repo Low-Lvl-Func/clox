94fb745a131f6e49e7830eac7a19bdbbeb2c37f1
StanaAndrei <stadey33@gmail.com>
2026-08-04 03:29:44 +0300

    added disasm

--- src/main.c
+++ src/main.c
@@ -1,7 +1,15 @@
 #include "common.h"
 #include <stdio.h>
+#include "chunk.h"
+#include "debug.h"
+
 
 int main(int argc, const char* argv[]) {
     puts("Hi!");
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
