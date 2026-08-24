a5bcfa2bf0772f6701580d7cd5fbbf0ae014c30e
StanaAndrei <stadey33@gmail.com>
2026-08-24 23:57:32 +0300

    better time to collect

--- src/memory.c
+++ src/memory.c
@@ -4,19 +4,23 @@
 #include "memory.h"
 #include "vm.h"
 
-
 #ifdef DEBUG_LOG_GC
 #include <stdio.h>
 #include "debug.h"
 #endif
 
 #define FREE(type, pointer) reallocate(pointer, sizeof(type), 0)
+#define GC_HEAP_GROW_FACTOR 2
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    vm.bytesAllocated += newSize - oldSize;
     if (newSize > oldSize) {
 #ifdef DEBUG_STRESS_GC
         collectGarbage();
 #endif
+        if (vm.bytesAllocated > vm.nextGC) {
+            collectGarbage();
+        }
     }
     if (newSize == 0) {
         free(pointer);
@@ -169,14 +173,20 @@ static void sweep() {
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
     printf("-- gc begin\n");
+    size_t before = vm.bytesAllocated;
 #endif
     markRoots();
     traceReferences();
     tableRemoveWhite(&vm.strings);
     sweep();
 
+    vm.nextGC = vm.bytesAllocated * GC_HEAP_GROW_FACTOR;
+
 #ifdef DEBUG_LOG_GC
     printf("-- gc end\n");
+    printf("   collected %zu bytes (from %zu to %zu) next at %zu\n",
+       before - vm.bytesAllocated, before, vm.bytesAllocated,
+       vm.nextGC);
 #endif
 }
 
